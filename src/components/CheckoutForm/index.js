33f52ed739b5fb51acf176c4fabe06847f01af33
Umair maqbool <umairmaqbool.1181@gmail.com>
2020-08-10 18:59:41 +0500

    fix

--- src/components/CheckoutForm/index.js
+++ src/components/CheckoutForm/index.js
@@ -89,7 +89,7 @@ class CheckoutForm extends React.Component {
       //     Hello WORLD
       //   </div>
       <div style={{ display: "flex" , justifyContent: "center"}} >
-        <button className={ "pay-button" }  disabled={!this.props.stripe} >Pay Now </button>
+        <button className={ "pay-button" } >Pay Now </button>
       </div>
       //     <div className="centered">
       //        <div style={{ display: "flex" , justifyContent: "center"}} >
