e57788e1a86966b7df5d8100bce105f7355a3f85
Umair maqbool <umairmaqbool.1181@gmail.com>
2020-08-11 08:17:00 +0500

    Updates

---
+++ precache-manifest.2e5c4a943a1431d15814a9d0d5887500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee62c778cedfce5778fb0adff931191",
+    "url": "/strpepay/index.html"
+  },
+  {
+    "revision": "7cd91391594867cf2751",
+    "url": "/strpepay/static/css/main.434de980.chunk.css"
+  },
+  {
+    "revision": "76acf0a636387dc5e248",
+    "url": "/strpepay/static/js/2.d309d7d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/strpepay/static/js/2.d309d7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd91391594867cf2751",
+    "url": "/strpepay/static/js/main.4cbf8ba1.chunk.js"
+  },
+  {
+    "revision": "91f432e74b1317d4b8bc",
+    "url": "/strpepay/static/js/runtime-main.aec86d6f.js"
+  },
+  {
+    "revision": "dc7fb8557434cc1311050c22117eb08d",
+    "url": "/strpepay/static/media/LoginScreenLogo.dc7fb855.png"
+  }
+]);
